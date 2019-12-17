64a14d4abffe91ff80140c67e39878e1a5ec41e6
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2019-12-17 11:56:39 +0200

    code_refactoring

--- server/entry.js
+++ server/entry.js
@@ -27,24 +27,7 @@ mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-
-  // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors());
 
 app.use(express.static(`${__dirname }/uploads`));
 
