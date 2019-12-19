fafd392fde35fa01e283f98505ba0dd360d1ad00
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2019-12-19 09:30:50 +0200

    refactoring_mongo

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/zarobitchanu", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/zarobitchanu",
+  { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.Promise = global.Promise;
 
