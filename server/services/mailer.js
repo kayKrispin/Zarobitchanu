125b9cb039f60b6df3fd9bbdda5e30f0ea2ff57f
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2019-12-19 17:11:03 +0200

    refactoring

--- server/services/mailer.js
+++ server/services/mailer.js
@@ -1,17 +1,18 @@
 const nodemailer = require("nodemailer");
 const config = require("../config");
 
-console.log(config)
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  secure: false,//true
+  port: 25,//465
   auth: {
-    user: "tarasbashuryn@gmail.com",
-    pass: "kaecxuec1993"
+    user: "taras.b@coaxsoft.com",
+    pass: "z1c3b5qq"
   }
 });
 
 const verifyEmail = mailOptions => {
+  console.log(mailOptions)
   return transporter.sendMail(mailOptions, function(error, info) {
     if (error) {
       console.log(error);
